63331bc303d2ce95f8db8a6166a5188f177dd39d
fiyan123 <moehamadsofyan@gmail.com>
2025-09-13 11:03:25 +0700

    first api smk

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = [
+            ['id' => 1, 'name' => 'Product 1', 'price' => 100],
+            ['id' => 2, 'name' => 'Product 2', 'price' => 200],
+            ['id' => 3, 'name' => 'Product 3', 'price' => 300],
+        ];
+
+        // $products = Product::all();
+
+        return response()->json([
+            'status' => 200,
+            'success' => true,
+            'message' => 'Products retrieved successfully',
+            'data' => $products,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
